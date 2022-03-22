7c989178ba8dd1181cb70f3d0b68dfddab4d28bf
JoseFV1999 <jose.falconi@tecsup.edu.pe>
2022-03-22 18:17:36 -0500

    Laboratorio1 modificacion

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,6 +1,15 @@
-function ArregloIzquierda(Desplazamiento){
+function ArregloIzquierda(){
+    let cantidad = prompt("Ingrese la cantidad de elementos de su lista");
+    let arreglo = [];
+    var cont = 0;
+    while (cont < cantidad){
+        let numero = prompt("Ingrese el numero "+(cont+1))
+        arreglo.push(numero)
+        cont = cont+1;
+        console.log(cont)
+    }
+    alert("El arreglo ingresado es: "+arreglo);
     var Desplazamiento = prompt("Cantidad de desplazamientos: ");
-    var arreglo = [1,2,3,4,5];
     for (var i = 0; i < Number(Desplazamiento); i++){
         var ultimo = arreglo.shift();
         arreglo.push(ultimo)
